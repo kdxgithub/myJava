3e383119059a6df8a125cef09b97792cb4a5fc73
kdx <769458363@qq.com>
2019-08-26 00:17:01 +0800

    dict test报账

--- dict/src/main/java/com/dict/demo/controller/ProductController.java
+++ dict/src/main/java/com/dict/demo/controller/ProductController.java
@@ -41,4 +41,9 @@ public class ProductController {
     public String item(){
         return "item";
     }
+
+    @RequestMapping(value = "test")
+    public String test(){
+        return "test";
+    }
 }
